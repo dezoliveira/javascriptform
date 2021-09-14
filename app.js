98e9a06715f26698d6fedb255202a24a31590094
Andrés Oliveira <andresoliveira@protonmail.com>
2021-09-14 13:46:39 -0300

    Ajusta código do Form

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const username = document.getElementById('username')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const password2 = document.getElementById('password2')
+let msgError = document.querySelector('#msgError')
+let msgSuccess = document.querySelector('#msgSuccess')
 
 let validUsername = false
 let validEmail = false
@@ -11,17 +13,14 @@ let validPassword2 = false
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    checkInputs()
     isValid()
 })
-
-let msgError = document.querySelector('#msgError')
-let msgSuccess = document.querySelector('#msgSuccess')
 
 username.addEventListener('keyup', () =>{
 
     if(username.value.length < 6 || username.value.length == ''){
         setErrorFor(username, 'Usuario deve possuir no mínimo 6 caracteres!')
+        validUsername = false
     } else{
         setSuccessFor(username)
         validUsername = true
@@ -32,12 +31,14 @@ username.addEventListener('blur', () =>{
 
     if(username.value === ''){
         setErrorFor(username, 'Usuário não pode ser vazio!')
+        validUsername = false
     }
 })
 
 email.addEventListener('keyup', () =>{
-    if(!isEmail(email.value)){
+    if(!isEmail(email.value) || email.value == ''){
         setErrorFor(email, 'Digite um email válido!')
+        validEmail = false
     }else{
         setSuccessFor(email)
         validEmail = true
@@ -48,13 +49,15 @@ email.addEventListener('blur', () =>{
 
     if(email.value === ''){
         setErrorFor(email, 'Email não pode ser vazio!')
+        validEmail = false
     }
 })
 
 password.addEventListener('keyup', () =>{
 
-    if(password.value.length < 7){
+    if(password.value.length < 8 || password.value == ''){
         setErrorFor(password, 'Senha deve ter no mínimo 8 caracteres!')
+        validPassword = false
     }else{
         setSuccessFor(password)
         validPassword = true
@@ -65,13 +68,15 @@ password.addEventListener('blur', () =>{
 
     if(password.value === ''){
         setErrorFor(password, 'Senha não pode ser vazia!')
+        validPassword = false
     }
 })
 
 password2.addEventListener('keyup', () =>{
 
-    if(password2.value != password.value){
+    if(password2.value != password.value || password2.value == ''){
         setErrorFor(password2, 'Senhas não correspondem!')
+        validPassword2 = false
     }
     else{
         setSuccessFor(password2)
@@ -83,48 +88,9 @@ password2.addEventListener('blur', () =>{
 
     if(password2.value === ''){
         setErrorFor(password2, 'Senha não pode ser vazia!')
+        validPassword2 = false
     }
 })
-
-function checkInputs(){
-    //Dá um get nos inputs
-    const usernameValue = username.value.trim()
-    const emailValue = email.value.trim()
-    const passwordValue = password.value.trim()
-    const password2Value = password2.value.trim()
-
-    if(usernameValue === ''){
-        setErrorFor(username, 'Usuário não pode ser vazio!')
-    } else{
-        setSuccessFor(username)
-    }
-
-    if(emailValue === ''){
-        setErrorFor(email, 'Email não pode ser vazio!')
-    } else if(!isEmail(emailValue)){
-        setErrorFor(email, 'Email não é valido!')
-    } else{
-        setSuccessFor(email)
-    }
-
-    if(passwordValue === ''){
-        setErrorFor(password, 'Senha não pode ser vazia!')
-    }else{
-        setSuccessFor(password)
-    }
-
-    if(password2Value === ''){
-        setErrorFor(password2, 'Senha não pode ser vazia!')
-    }
-
-    else if(passwordValue !== password2Value){
-        setErrorFor(password2, 'As senhas não correspondem!')
-    }
-
-    else{
-        setSuccessFor(password2)
-    }
-}
 
 function isValid(){
     if(validUsername && validEmail && validPassword && validPassword2){
@@ -155,7 +121,7 @@ function setErrorFor(input, message){
 }
 
 function setSuccessFor(input){
-    const formControl = input.parentElement
+    const formControl = input.parentElement 
     formControl.className = 'form-control success'
 }
 
